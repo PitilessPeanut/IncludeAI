bae4b8610d667aa11e53d41cabe6c95a385d4fb0
Shaiden Spreitzer <professorpeanut.unpaid@simplelogin.fr>
2026-05-20 12:01:23 +0800

    get rid of getBoardScore() - PART II

--- src/ai.hpp
+++ src/ai.hpp
@@ -771,7 +771,7 @@ int shallowestTerminalDepth = 9999;
                   const SWORD polarity = boardClone.getWinner()!=boardOriginal.getCurrentPlayer() ? -1 : 1;
                 #endif
 
-                const FLOAT *pValues = nn.evaluate(boardClone.getNetworkInputs(), boardClone.getBoardScore());
+                const FLOAT *pValues = nn.evaluate(boardClone.getNetworkInputs());
                 //const FLOAT *pValues = selectedNode->nnEvaluationResult;
                 const FLOAT confidence = pValues[0];
                 aiAssert(confidence<1.1f && confidence>-1.1f);
